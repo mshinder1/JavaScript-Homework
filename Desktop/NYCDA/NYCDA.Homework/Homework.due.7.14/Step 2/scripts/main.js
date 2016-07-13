94a126741bccc0d5043f144980f0d831ed766d0f
madeleine shinder <madeleineshinder@gmail.com>
2016-07-13 17:35:01 -0400

    I just finished step 2 and am going to start the 5th step

--- Desktop/NYCDA/NYCDA.Homework/Homework.due.7.14/Step 2/scripts/main.js
+++ Desktop/NYCDA/NYCDA.Homework/Homework.due.7.14/Step 2/scripts/main.js
@@ -7,3 +7,13 @@ function alertName(z){
 	return alert(z);
 }
 alertName("This is your last alert on this page...:)");
+
+
+function aName (aNumber, anotherNumber){
+	return aNumber + 10 + anotherNumber;	
+}
+var aNumber = 7
+
+var anotherNumber = 2
+
+console.log(aName(7, 2));
