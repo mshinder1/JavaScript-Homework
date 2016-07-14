e3a7cbab5bbda932d776c8a96561729821210461
madeleine shinder <madeleineshinder@gmail.com>
2016-07-14 14:33:50 -0400

    I finished steps 3 and 5. I only have to do two more steps

---
+++ Desktop/NYCDA/NYCDA.Homework/Homework.due.7.14/Step.3/scripts/main.js
@@ -0,0 +1,11 @@
+var x = 30;
+
+var y = 12;
+
+console.log(x + y);
+
+var firstName = "Madeleine";
+
+var lastName = "Shinder";
+
+console.log(firstName + " " + lastName);
---
+++ Desktop/NYCDA/NYCDA.Homework/Homework.due.7.14/Step.5/scripts/main.js
@@ -0,0 +1,7 @@
+var x = 7;
+
+if(x<10){
+	alert('Your variable is less than 10!');
+}else{
+	console.log("The variable was 7. 7 is less than 10. But you shouldn't see this.");
+};
