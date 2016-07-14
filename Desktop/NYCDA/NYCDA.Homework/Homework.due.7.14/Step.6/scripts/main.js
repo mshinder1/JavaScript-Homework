4b0597b410608092186c15d780a2612c3ef4c95a
madeleine shinder <madeleineshinder@gmail.com>
2016-07-14 15:10:54 -0400

    I finished step 6 and am going to go to the final step

---
+++ Desktop/NYCDA/NYCDA.Homework/Homework.due.7.14/Step.6/scripts/main.js
@@ -0,0 +1,7 @@
+var x = 12;
+
+if(x<10){
+	alert("Your variable is less than 10! But you shouldn't see this...");
+}else{
+	console.log("The variable was 12. 12 is greater than 10. You should see this.");
+};
